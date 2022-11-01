c4be9340c6c46b49b5673208319d0c69f551199e
Rigoberto L. Perez <rigperez@proton.me>
2022-10-31 23:39:23 -0400

    feat: use gql for title

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,13 +1,34 @@
 import { Container } from "@chakra-ui/react";
+import { Link, useStaticQuery, graphql } from "gatsby";
 import React from "react";
 import Footer from "./footer";
 import Header from "./header";
 
-const Layout = ({ pageTitle, children }) => {
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
   return (
     <>
-      <Header pageTitle={pageTitle} />
+      <Header pageTitle={data.site.siteMetadata.title} />
       <Container maxW="container.md" paddingY={5} marginBottom="12">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>,
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
         {children}
       </Container>
       <Footer />
