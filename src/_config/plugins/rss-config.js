ed4b4d7bbb599c859e6c060d8c13ada565fed2e9
Rigoberto Perez <rlperez@kablamo.me>
2024-09-10 21:31:58 -0400

    fix: rss wrong in diff envs (#7)

---
+++ src/_config/plugins/rss-config.js
@@ -0,0 +1,24 @@
+import {feedPlugin} from '@11ty/eleventy-plugin-rss';
+import {url} from '../../_data/meta.js';
+
+export const rssConfig = eleventyConfig => {
+  eleventyConfig.addPlugin(feedPlugin, {
+    type: 'atom', // or "rss", "json"
+    outputPath: '/feed.xml',
+    collection: {
+      name: 'posts',
+      limit: 15
+    },
+    metadata: {
+      language: 'en',
+      title: 'Kablamo.me',
+      subtitle:
+        'A personal blog on technology and software development. Or whatever other' +
+        ' interesting projects I undertake that are worth sharing.',
+      base: url(),
+      author: {
+        name: 'Rigoberto L. Perez'
+      }
+    }
+  });
+};
