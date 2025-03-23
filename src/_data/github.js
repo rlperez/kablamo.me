103e5ff64805dedab83feeb5ba33713345309ad8
Rigoberto L. Perez <rlperez@kablamo.me>
2025-03-23 01:14:09 -0400

    🐛 fix: add the functino to fetch at least some parts of the github finer details

--- src/_data/github.js
+++ src/_data/github.js
@@ -9,5 +9,29 @@ export default async function () {
     type: 'json'
   });
 
+  console.error({data});
+
   return data;
+}
+
+/**
+ * Takes a repo returned from the user repos returned from
+ * the GitHub api and returns details from the repo.
+ */
+export async function getRepoDetails(repo) {
+  const [latest_release, languages] = await Promise.allSettled([
+    Promise.resolve(repo.name),
+    Fetch(repo.releases_url, {id: 'latest'}),
+    Fetch(repo.languages_url)
+  ]);
+
+  console.error(latest_release);
+
+  return {
+    name: repo.name,
+    html_url: repo.html_url,
+    description: repo.description,
+    latest_release,
+    languages
+  };
 }
